1c499fa0b9deadad425696429a258342897e88bc
hum-sc <humbertoantsc@gmail.com>
2020-10-23 20:35:27 -0500

    Updates

---
+++ precache-manifest.1ee86ae30b4473a96ac983123c3962d4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5bfbafc8d3dbd18f175d7ea9fc93bef",
+    "url": "/opli/index.html"
+  },
+  {
+    "revision": "7dd6f3c2ad7ca2709413",
+    "url": "/opli/static/css/main.33562c8d.chunk.css"
+  },
+  {
+    "revision": "60d542d0264b10fcbf6e",
+    "url": "/opli/static/js/2.50f6e702.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/opli/static/js/2.50f6e702.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dd6f3c2ad7ca2709413",
+    "url": "/opli/static/js/main.acf99142.chunk.js"
+  },
+  {
+    "revision": "c3ba4fc7952526061022",
+    "url": "/opli/static/js/runtime-main.9ccacdd6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/opli/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5613db24b64483f4b5f68fd55cc50f26",
+    "url": "/opli/static/media/menu-white.5613db24.svg"
+  }
+]);
